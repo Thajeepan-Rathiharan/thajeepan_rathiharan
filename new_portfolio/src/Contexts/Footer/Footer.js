8c23e2da6f588b136419c4b53dd2f652a07b9976
Thajeepan-Rathiharan <thajee2002@gmail.com>
2024-04-06 10:18:08 +0100

    Added footer.js

---
+++ new_portfolio/src/Contexts/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Thajeepan-Rathiharan/thajeepan_rathiharan'
+      className='link footer__link'
+    >
+      Created By Cheng Chien Huang
+    </a>
+  </footer>
+)
+
+export default Footer
